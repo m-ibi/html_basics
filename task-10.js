1ef343f69061f60a8c5ce74ae25dde9de11f6906
ibrahim ahmed <m.ibi.ahmed@gmail.com>
2022-06-18 15:06:33 +0500

    task10commit

--- task-10.js
+++ task-10.js
@@ -42,3 +42,20 @@ var ctx = cnv.getContext("2d" ) ;
    document.getElementById('clear').addEventListener('click', function () {
     ctx.clearRect(0, 0, grid.width, grid.height);
 }, false);
+
+
+var body = document.getElementsByTagName("body")[0];
+var canvas = document.createElement("canvas");
+
+body.appendChild(canvas);
+function create() {
+//  Opacity
+    ctx.globalAlpha=0.7;
+    var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
+    ctx.fillStyle = color;
+
+    //Each rectangle's size is (20 ~ 100, 20 ~ 100)     
+    ctx.fillRect(Math.random()*canvas.width, Math.random()*canvas.width, Math.random()*80+20, Math.random()*80+20);
+
+}
+ document.getElementById('buttonxD').addEventListener('click', create);
