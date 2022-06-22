f37b3d5ac32dea1e548ad70bb1e16edf3a4704f0
ibrahim ahmed <m.ibi.ahmed@gmail.com>
2022-06-22 20:56:07 +0500

    task2 fabric done

--- task-12.js
+++ task-12.js
@@ -1,6 +1,16 @@
-
-
 var canvas = new fabric.Canvas('canvas');
+var r = undefined;
+
+ function mouseOver(e) {
+  e.target.set('fill', 'red');
+  canvas.renderAll();
+};
+
+function mouseOut(e) {
+  e.target.set('fill', 'green');
+  canvas.renderAll();
+}
+
 
 function newObject(){
 var rect = new fabric.Rect({
@@ -14,26 +24,44 @@ height:Math.random()*80+20,
 
 canvas.add(rect);
 
-
-canvas.on('mouse:over', function(e) {
-    e.target.set('fill', 'red');
-    canvas.renderAll();
-  });
-
-  canvas.on('mouse:out', function(e) {
-    e.target.set('fill', 'green');
-    canvas.renderAll();
-  });
-
-
+rect.on('mouseover',mouseOver) ;
+rect.on('mouseout',mouseOut) ;
+rect.on('mouseup',mouseclick);
+canvas.renderAll()
 }
 
 
+function newObjectCircle(){
+  var circle = new fabric.Circle({
+  
+  left:100,
+  top:Math.random()*canvas.height, 
+  fill:'#'+ Math.round(0xffffff * Math.random()).toString(16),
+  radius:Math.random()*canvas.height,
+  })
+  
+  canvas.add(circle);
+  circle.on('mouseover',mouseOver) 
+  circle.on('mouseout',mouseOut) 
+  circle.on('mouseup',mouseclick);
+canvas.renderAll()
+
+}
+
+function mouseclick(e) {
+  allObjects = canvas.getObjects()
+  r = allObjects[2]
+  console.log("here") ;
+   
+  if (allObjects.length > 1) {
+     console.log(allObjects[2])
+    //  e.target.set('left', 20)
+    allObjects[2].set('left', e.target.get('left'))
+   }
+     
+    
+};
+
 document.getElementById('btn').addEventListener('click',newObject);
-
+document.getElementById('btn2').addEventListener('click',newObjectCircle);
 newObject()
-mouseOver()
-document.getElementById('clear').addEventListener('click', function clear () {
-    ctx.clearRect(0, 0, grid.width, grid.height);
-}, false);
-
