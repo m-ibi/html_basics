2e3d4a45b0ba6bb07802ac106ff0085d7bc576b6
ibrahim ahmed <m.ibi.ahmed@gmail.com>
2022-06-23 21:53:29 +0500

    formatted js

--- task-13.js
+++ task-13.js
@@ -1,18 +1,43 @@
 var canvas = new fabric.Canvas('canvas');
 var r = undefined;
-
 var car = undefined;
+var acc = 0;
+var brake = 0;
+var steer = 0;
 
- function mouseOver(e) {
-  e.target.set('fill', 'red');
-  canvas.renderAll();
-};
-
-function mouseOut(e) {
-  e.target.set('fill', 'green');
-  canvas.renderAll();
+window.addEventListener("onload", onload);
+function onload(){
+    console.log("testing");
 }
 
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+} 
+
+docReady(function() {
+    console.log("Doc Ready function..");
+    // now adding btn events
+    document.getElementById('btn1').addEventListener('click',newObject);
+    document.getElementById('btn2').addEventListener('click',newObjectCircle);
+    document.getElementById('btn3').addEventListener('click',addCar);
+
+});
+
+function updateScreen(){
+    a = document.getElementById("acc");
+    b = document.getElementById("brake");
+    c = document.getElementById("steer");
+    a.innerHTML = "acc:"+String(acc);
+    b.innerHTML = "brake:"+String(brake);
+    c.innerHTML = "steer:"+String(steer);
+}
+//-------------------------------------//
+//---adding objects---//
 
 function newObject(){
 var rect = new fabric.Rect({
@@ -34,16 +59,10 @@ canvas.renderAll()
 
 function addCar(){
 
-    // approach 1 ( failing, as data loading confirmation error)
-    // car = new fabric.Image.fromURL('library/car1.png');
-    // canvas.add(car);
-    // canvas.renderAll()
-    // approach 2 ( hopefully will work)
+   
     fabric.Image.fromURL('library/car1.png', function (img){
         canvas.add(img);
         console.log(canvas.getObjects());
-        // console.log(car);
-        // return img;
     });
 }
 
@@ -57,7 +76,6 @@ function newObjectCircle(){
   fill:'#'+ Math.round(0xffffff * Math.random()).toString(16),
   radius:Math.random()*canvas.height,
   })
-  
   canvas.add(circle);
   circle.on('mouseover',mouseOver) 
   circle.on('mouseout',mouseOut) 
@@ -66,6 +84,21 @@ canvas.renderAll()
 
 }
 
+//----------------//
+//--mouseEvents---//
+//-----------------//
+
+
+
+function mouseOver(e) {
+    e.target.set('fill', 'red');
+    canvas.renderAll();
+  };
+  
+  function mouseOut(e) {
+    e.target.set('fill', 'green');
+    canvas.renderAll();
+  }
 function mouseclick(e) {
   allObjects = canvas.getObjects()
   r = allObjects[2]
@@ -75,65 +108,15 @@ function mouseclick(e) {
      console.log(allObjects[2])
     //  e.target.set('left', 20)
     allObjects[2].set('left', e.target.get('left'))
-   }
-     
-    
+   }  
 };
 
-window.addEventListener("onload", onload);
 
-function onload(){
-    console.log("testing");
-}
-
-
-
-// safe init..
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-} 
-
-docReady(function() {
-    console.log("Doc Ready function..");
-    // now adding btn events
-    document.getElementById('btn1').addEventListener('click',newObject);
-    document.getElementById('btn2').addEventListener('click',newObjectCircle);
-    document.getElementById('btn3').addEventListener('click',addCar);
-    // keyboard events are best done in window level (below is failing)
-    // canvas.on('keydown', checkKeyPress,false);
-
-});
 //--------------------------//
 //---------KEYBOARD EVENTS  //
 //--------------------------//
 
 
-// op1
-// window.addEventListener('keydown',(event) => {     
-//     console.log(event)
-// });
-
-// op2 (better because function with name is separate thing)
-
-
-var acc = 0;
-var brake = 0;
-var steer = 0;
-
-function updateScreen(){
-    a = document.getElementById("acc");
-    b = document.getElementById("brake");
-    c = document.getElementById("steer");
-    a.innerHTML = "acc:"+String(acc);
-    b.innerHTML = "brake:"+String(brake);
-    c.innerHTML = "steer:"+String(steer);
-}
 
 window.addEventListener('keydown', checkKeyPressDown,false);
 function checkKeyPressDown(key){
