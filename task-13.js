e73f7dada79e2b5a3ccefe322bfeb3784ad4edf4
ibrahim ahmed <m.ibi.ahmed@gmail.com>
2022-06-25 15:06:29 +0500

    starting tic tac toe car project done

--- task-13.js
+++ task-13.js
@@ -1,9 +1,11 @@
 var canvas = new fabric.Canvas('canvas');
 var r = undefined;
-var car = undefined;
-var acc = 0;
+var car = null;
+var acc = false;
 var brake = 0;
 var steer = 0;
+var speed = 0;
+var drag = 0.05;
 
 window.addEventListener("onload", onload);
 function onload(){
@@ -24,15 +26,17 @@ docReady(function() {
     // now adding btn events
     document.getElementById('btn1').addEventListener('click',newObject);
     document.getElementById('btn2').addEventListener('click',newObjectCircle);
-    document.getElementById('btn3').addEventListener('click',addCar);
+    //document.getElementById('btn3').addEventListener('click',addCar);
 
 });
 
 function updateScreen(){
     a = document.getElementById("acc");
+    s = document.getElementById("speed");
     b = document.getElementById("brake");
     c = document.getElementById("steer");
     a.innerHTML = "acc:"+String(acc);
+    s.innerHTML = "speed:"+String(Math.round(speed * 10) / 10);
     b.innerHTML = "brake:"+String(brake);
     c.innerHTML = "steer:"+String(steer);
 }
@@ -45,28 +49,33 @@ var rect = new fabric.Rect({
 left:100,
 top:Math.random()*canvas.height, 
 fill:'#'+ Math.round(0xffffff * Math.random()).toString(16),
-width:Math.random()*canvas.width ,
-height:Math.random()*80+20,
+width:50 ,
+height:50 ,
+angle:-90
+
 })
 
 canvas.add(rect);
 
 rect.on('mouseover',mouseOver) ;
 rect.on('mouseout',mouseOut) ;
-rect.on('mouseup',mouseclick);
+rect.on('keydown', checkKeyPressDown);
+rect.on('keyup', checkKeyPressUp);
 canvas.renderAll()
 }
-
-function addCar(){
+newObject();
+var car = canvas.getObjects()[0];
+//function addCar(){
 
    
-    fabric.Image.fromURL('library/car1.png', function (img){
-        canvas.add(img);
-        console.log(canvas.getObjects());
-    });
-}
-
-
+     // car = new fabric.Image.fromURL('library/car1.png', function (img){
+       
+        
+       //  canvas.add(img);
+        
+        //console.log(canvas.getObjects());
+    //});
+//}
 
 function newObjectCircle(){
   var circle = new fabric.Circle({
@@ -80,12 +89,15 @@ function newObjectCircle(){
   circle.on('mouseover',mouseOver) 
   circle.on('mouseout',mouseOut) 
   circle.on('mouseup',mouseclick);
-canvas.renderAll()
 
+steerLeft();
+steerRight();
+//updateRect();
+canvas.renderAll()
 }
 
 //----------------//
-//--mouseEvents---//
+//---mouseEvents---//
 //-----------------//
 
 
@@ -124,25 +136,31 @@ function checkKeyPressDown(key){
     console.log('keydown');
    
     if (key.key == 'w') {
-        acc = 1
+        acc = true
         //console.log("keyW")
-        updateScreen()
+        // updateScreen()
+        updateRect()
+        // canvas.renderAll()
     }
     if (key.key == 'a') {
         steer = 'left'
         //console.log("keyA")
-        updateScreen()
+        // updateScreen()
+        steerLeft()
+        // canvas.renderAll()
     }
     if (key.key == 's') {
         brake = 1
         //console.log("keyS")
-        updateScreen()
+        // updateScreen()
     }
     if (key.key == 'd') {
         steer = 'right'
         //console.log("keyD")
-        updateScreen()
+        steerRight()
+        // canvas.renderAll()
     }
+    updateScreen()
 
 
 }
@@ -155,25 +173,75 @@ function checkKeyPressUp(key){
     if (key.key == "w") {
         acc = 0
        // console.log("keyW")
-       updateScreen()
+    //    updateScreen()
     }
     if (key.key == "a") {
         steer = 0;
        // console.log("keyA")
-       updateScreen()
+    //    updateScreen()
     }
     if (key.key == "s") {
         brake = 0;
        // console.log("keyS")
-       updateScreen()
+    //    updateScreen()
     }
     if (key.key == "d") {
         steer = 0;
         //console.log("keyD")
-        updateScreen()
     }
+    updateScreen()
 
 
+
+}
+
+//-------------------------------//
+//---------car functions---------//
+//-------------------------------//
+
+ var d = canvas.getObjects()[0];
+
+  
+function steerRight(){
+   
+   a = canvas.getObjects()[0];
+    a.rotate(a.angle+10);
+    canvas.renderAll()
+
+}
+
+function steerLeft(){
+    
+    a = canvas.getObjects()[0];
+    a.rotate(a.angle-10);
+    canvas.renderAll()
+ 
+ 
+ }
+
+ function updateRect(){
+
+    
+    car.left+= speed * Math.cos(car.angle*Math.PI/180)*drag;
+    car.top += speed * Math.sin(car.angle*Math.PI/180)*drag;
+    canvas.renderAll()
+}
+
+setInterval(onTimerTick,33);
+
+
+function onTimerTick(){
+   
+   if(acc==true){
+    console.log("increasing speed");
+    speed += 1; 
+   } else{
+    console.log("stopping due to friction/drag");
+      speed -= speed * drag;
+   }
+   updateScreen();
+   updateRect();
+   //canvas.renderAll();
 
 }
 
